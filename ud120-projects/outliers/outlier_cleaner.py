32930ba2ecf72321b1283f20fee94f88f6b7fd53
fan3 <wr_great@163.com>
2018-04-04 15:12:05 +0800

    update p1

--- ud120-projects/outliers/outlier_cleaner.py
+++ ud120-projects/outliers/outlier_cleaner.py
@@ -10,8 +10,17 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+    error=[]
     cleaned_data = []
+    for i in range(0,len(predictions)):
+        error.append(net_worths[i]-predictions[i])
+        cleaned_data.append((ages[i],net_worths[i],error[i]))
+    errabs=map(abs,error)
+    for _ in range(0,int(0.1*len(error))):
+        pop_index=errabs.index(max(errabs))
+        errabs.pop(pop_index)
+        cleaned_data.pop(pop_index)
+
 
     ### your code goes here
 
--- ud120-projects/validation/validate_poi.py
+++ ud120-projects/validation/validate_poi.py
@@ -14,6 +14,8 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -24,9 +26,13 @@ features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
+features_train,features_test,labels_train,labels_test=train_test_split(features,labels,test_size=1,random_state=42)
 
 
 
 ### it's all yours from here forward!  
-
+from sklearn import tree
+clf=tree.DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+print("accuracy:",accuracy_score(clf.predict(features_train),labels_train))
 
